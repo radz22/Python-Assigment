c9b4576d02215035e735d40f52dc50aef135826e
radz22 <radzsantillan01@gmail.com>
2025-02-05 22:13:03 +0800

    Fahrenheit to Celsius and Celsius to Fahrenheit

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,14 @@
 
 
-choice = int(input("Choose Conversion:\n1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit\nEnter your choice: "))
+choiceFahrenheitAndCelsius = int(input("Choose Conversion:\n1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit\nEnter your choice: "))
 
 
-if choice == 1:
+if choiceFahrenheitAndCelsius == 1:
     fahrenheit = float(input("Enter temperature in Fahrenheit: "))
     convert = (fahrenheit - 32) / 1.8
     print(f"Temperature in Celsius: {convert:.2f}")  
 
-elif choice == 2:
+elif choiceFahrenheitAndCelsius == 2:
     celsius = float(input("Enter temperature in Celsius: "))
     convert = (celsius * 1.8) + 32
     print(f"Temperature in Fahrenheit: {convert:.2f}") 
